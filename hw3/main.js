bcd49b7acc9ac21e04aea8f2c4843801ac2cf57a
doris31109861 <95745651+doris31109861@users.noreply.github.com>
2023-04-24 22:32:02 +0800

    hw3

---
+++ hw3/main.js
@@ -0,0 +1,11 @@
+document.getElementById("submit").addEventListener("click", function() {
+    // 取得使用者輸入的帳號和密碼
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // 進行登入驗證，可以使用後端伺服器或其他方式
+    // ...
+
+    // 登入成功後，導向到YouTube網址
+    window.location.href = "https://youtu.be/_Bz8WA9TpsQ";
+});
